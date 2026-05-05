b23dbee0c50076b11cf6d0f83d59b9236e8b2e4e
remmy <sayeakinyi@gmail.com>
2026-05-05 20:24:43 +0300

    fix(infra): Increase ACM certificate validation timeout to 45m as DNS propagation usually exceeds the previous 5m limit

--- infra/modules/acm/main.tf
+++ infra/modules/acm/main.tf
@@ -21,7 +21,7 @@ resource "aws_acm_certificate_validation" "filesoncloud_cert_validation" {
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 
   timeouts {
-    create = "5m"
+    create = "45m"
   }
 }
 
