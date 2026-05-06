b3e41d29bc1e4535164cb26aec53fc221bd12a70
remmy <sayeakinyi@gmail.com>
2026-05-06 20:59:42 +0300

    fix(infra): Remove API Gateway VPC Interface endpoint that's overriding DNS resolution within the VPC, routing ECS task traffic through the endpoint instead of the public API

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -314,21 +314,6 @@ resource "aws_vpc_endpoint" "logs" {
   }
 }
 
-# API Gateway VPC Endpoint
-resource "aws_vpc_endpoint" "apigateway" {
-  vpc_id              = aws_vpc.filesoncloud_vpc.id
-  service_name        = "com.amazonaws.${var.aws_region}.execute-api"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-
-  tags = {
-    Name        = "${var.project_name}-apigateway-endpoint"
-    Environment = var.environment
-  }
-}
-
 # S3 Gateway endpoint for ECR
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.filesoncloud_vpc.id
